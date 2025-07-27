0a9d31f476ed50efb4e25fc2e557f961ecf94546
David-396 <david.eini12@gmail.com>
2025-07-27 10:39:52 +0300

    creating strting program

---
+++ start.sh
@@ -0,0 +1,12 @@
+docker network create server-classifier-network
+
+cd server_side
+docker build -t main_server__v2.0 .
+docker run -d -p 8003:8001 --network server-classifier-network --name cls_server_container__v2.0 cls_server__v2.0
+cd ..
+
+cd classifier_server
+docker build -t cls_server__v2.0 .
+docker run -d -p 8002:8000 --network server-classifier-network --name main_server_container__v2.0 main_server__v2.0
+cd ..
+
