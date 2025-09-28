7f616a498aac5168c8ccaa0b6f5bff3f8cdf1bcc
David-396 <david.eini12@gmail.com>
2025-09-28 14:52:46 +0300

    del remove packages

--- freeze_and_remove_packages.sh
+++
@@ -1,7 +0,0 @@
-.venv\Scripts\activate
-python -m pip install --upgrade --force-reinstall pip
-pip freeze > requirements.txt
-pip uninstall -r requirements.txt -y
-git add .
-git commit -m "adding remove packages file"
-git push
